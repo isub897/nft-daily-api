4e55a2842b8ec8144de3f0346baebfb71ca27087
isub897 <ivsubritzky@gmail.com>
2022-06-20 12:56:34 +1200

    uploading initial

--- server.mjs
+++ server.mjs
@@ -9,27 +9,15 @@ app.use(express.json());
 app.use(cors());
 
 
-const url = 'https://top-nft-collections-and-sales.p.rapidapi.com/collections/30d';
+const urlTNCAS = 'https://top-nft-collections-and-sales.p.rapidapi.com/collections/30d';
 
-const options = {
+const optionsTNCAS = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Key': '837dc6b60amsh7c39ed5f163c497p15734ejsn48d377555198',
       'X-RapidAPI-Host': 'top-nft-collections-and-sales.p.rapidapi.com'
     }
   };
-
-  
-  //   fetch(url, options)
-  //   .then(res => res.json())
-  //   .then(json => res.json(json))
-  //   .catch(err => console.error('error:' + err));
-  
-
-
-
-
-// respond with "hello world" when a GET request is made to the homepage
 
 app.get('/24hr', (req, res) => {timePeriods.twentyFour(req, res, fs)});
 app.get('/7d', (req, res) => {timePeriods.sevenDay(req, res, fs)});
